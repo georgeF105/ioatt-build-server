7e9f9afe068762ff16d27a07937fa753169c339f
george francis <georgefrancis105@gmail.com>
2017-09-30 11:44:26 +1300

    Change version

--- routes/builds.js
+++ routes/builds.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+let currentVersion = '0.0.2';
+
 /* GET builds. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -10,6 +12,16 @@ router.get('/', function(req, res, next) {
 /* GET builds. */
 router.get('/fetch/:build', function(req, res, next) {
   let build = req.params.build;
+  let deviceVersion = req.headers['x-esp8266-version'];
+  console.log('deviceVersion:', deviceVersion);
+
+  if (deviceVersion === currentVersion) {
+    console.log('update not needed!');
+    res.status(304).send('update not needed');
+    return;
+  }
+
+  console.log('Sending update');
   res.sendFile(path.join(__dirname, '../builds', build, `${build}.bin`));
 });
 
