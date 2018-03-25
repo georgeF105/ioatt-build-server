9a5c6a92542d9b201d9fb548d30d02cfc0a3622f
george francis <georgefrancis105@gmail.com>
2018-03-25 20:22:14 +1300

    Use gh release and make dev mode

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var builds = require('./routes/builds');
+var sensor = require('./routes/sensor');
 
 var app = express();
 
@@ -30,7 +31,8 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/builds', builds);
+app.use('/sensor', sensor);
+app.use('/firmware', builds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/builds.js
+++ routes/builds.js
@@ -1,28 +1,88 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const gh = require('ghreleases');
+
+const https = require('https');
+const fs = require('fs');
+const request = require('request');
 
 let currentVersion = '0.0.2';
 
+let devMode = true;
+let lastUpdated = Date.now();
+const minUpdateTime = 30000;
+
 /* GET builds. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
+  console.log('test headers', req.headers);
   res.send('respond with a resource');
 });
 
 /* GET builds. */
-router.get('/fetch/:build', function(req, res, next) {
-  let build = req.params.build;
+router.get('/:type', function(req, res, next) {
+  let type = req.params.type;
   let deviceVersion = req.headers['x-esp8266-version'];
   console.log('deviceVersion:', deviceVersion);
+  console.log('HEADERS:', req.headers);
 
-  if (deviceVersion === currentVersion) {
-    console.log('update not needed!');
-    res.status(304).send('update not needed');
-    return;
+  const auth = {
+    user: 'georgeF105',
+    token: '80588d9fa6b7d827eb5143465a2da7b707308db7'
+  };
+
+  if (!devMode) {
+    gh.getLatest(auth, 'georgeF105', 'ioatt-device', (err, release) => {
+      if (err) {
+        res.status(500).send(err);
+        console.log('err', err);
+        return;
+      }
+  
+      const { tag_name, assets } = release;
+      console.log('tag_name', tag_name);
+      console.log('release', release);
+          
+      if (deviceVersion === tag_name || true) {
+        console.log('update not needed!');
+        res.status(304).send('update not needed');
+        return;
+      }
+  
+      const asset = (assets || []).find(a => a.name === type);
+  
+      if (!asset) {
+        res.status(404).send(`Type ${type} not found`);
+        return;
+      }
+  
+      // var file = fs.createWriteStream(type);
+      // const fileReq = https.request(asset.browser_download_url, response => {
+      //   console.log('Sending update');
+      //   res.setHeader('content-disposition', `attachment; filename=${type}`);
+      //   res.setHeader('content-length', asset.size);
+      //   response.pipe(res);
+      // });
+      // fileReq.end();
+  
+      
+  
+  
+      res.setHeader('content-disposition', `attachment; filename=${type}`);
+      res.setHeader('content-length', asset.size);
+      request(asset.browser_download_url).pipe(res);
+    }); 
+  } else {
+    if (lastUpdated + minUpdateTime > Date.now()) {
+      console.log('update not needed!');
+      res.status(304).send('update not needed');
+    } else {
+      let _path = path.join(__dirname, '../../device', '.pioenvs', 'd1_mini', 'firmware.bin');
+      console.log('updating to', _path);
+      res.sendFile(_path);
+    }
   }
-
-  console.log('Sending update');
-  res.sendFile(path.join(__dirname, '../builds', build, `${build}.bin`));
+   
 });
 
 module.exports = router;
