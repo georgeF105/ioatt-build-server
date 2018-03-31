223d37c8b8d98e5a25e543bf6ccdf94c9b474d3c
george francis <georgefrancis105@gmail.com>
2018-03-31 14:47:39 +1300

    Add timestamp to device on update

--- src/services/firebase/firebase-admin.service.ts
+++ src/services/firebase/firebase-admin.service.ts
@@ -9,9 +9,14 @@ export class FirebaseAdminService {
   }
 
   public pushSensorUpdate (deviceName: string, data: any): Promise<any> {
+    this.updateDeviceLastComDate(deviceName);
     return this.database.ref('devices').child(deviceName).once('value').then(snapshot => {
       const { sensor } = snapshot.val(); ;
       return this.database.ref('sensors').child(sensor).child('data').push().set(data);
     });
+  }
+
+  private updateDeviceLastComDate (deviceName: string): void {
+    this.database.ref('devices').child(deviceName).child('lastComDate').set(admin.database.ServerValue.TIMESTAMP);
   }
 }
